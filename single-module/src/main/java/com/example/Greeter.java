60126ba2d85f613abf3ca8af91e5a32cc313e5d1
mjcifuentesr <56403953+mjcifuentesr@users.noreply.github.com>
2020-04-03 14:57:23 -0500

    Update Greeter.java

--- single-module/src/main/java/com/example/Greeter.java
+++ single-module/src/main/java/com/example/Greeter.java
@@ -14,10 +14,8 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    private int password=9;
-    private String pass="Hola";
-    
-    return String.format("Hello, %s!"+password+pass+"Contcat", someone);
+
+    return String.format("Hello, %s!", someone);
     
     
     
